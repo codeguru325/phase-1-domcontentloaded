d009454bfeba7e882cc2c084e400a228462314ae
samuel <sammykuria300@gmail.com>
2023-09-29 16:26:03 +0300

    Quick commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+const targetElement = document.getElementById("text")
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    targetElement.textContent = "This is really cool!"
+  });
